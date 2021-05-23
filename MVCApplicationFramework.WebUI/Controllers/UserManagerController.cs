1e3261a40b1d74e914123e9fb4fef9a613111e3b
Adarsh <adarsh.b@codearray.tech>
2021-05-23 17:25:09 +0530

    Updated Code

--- MVCApplicationFramework.WebUI/Controllers/UserManagerController.cs
+++ MVCApplicationFramework.WebUI/Controllers/UserManagerController.cs
@@ -47,15 +47,43 @@ namespace MVCApplicationFramework.WebUI.Controllers
         {
             using (var client = new HttpClient())
             {
-                var response = client.GetAsync(ConfigurationManager.AppSettings["WebAPIBaseURL"] + "UserManager/GetUserByID?userId" + Convert.ToString(Id)).Result;
+                var response = client.GetAsync(ConfigurationManager.AppSettings["WebAPIBaseURL"] + "UserManager/GetUserByID?userId=" + Convert.ToString(Id)).Result;
 
                 if (response.IsSuccessStatusCode && response.StatusCode == HttpStatusCode.OK)
                 {
                     var apiResponseObject = JsonConvert.DeserializeObject<APIResponse>(response.Content.ReadAsStringAsync().Result);
                     if (apiResponseObject.MessageType == EnumMessageType.OPERATION_SUCCESS)
                     {
-                        var user = JsonConvert.DeserializeObject<User>(apiResponseObject.Data[0].ToString());
+                        var user = JsonConvert.DeserializeObject<Entities.User>(apiResponseObject.Data[0].ToString());
                         return View("Edit", user);
+                    }
+                    else
+                    {
+                        throw new Exception(apiResponseObject.Exception);
+                    }
+                }
+                else
+                {
+                    log.Error("Error StatusCode:" + response.StatusCode.ToString() + " - " + response.ReasonPhrase);
+                    throw new Exception(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Entities.User model)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsync(ConfigurationManager.AppSettings["WebAPIBaseURL"] + "UserManager/Save", new StringContent(JsonConvert.SerializeObject(model))).Result;
+
+                if (response.IsSuccessStatusCode && response.StatusCode == HttpStatusCode.OK)
+                {
+                    var apiResponseObject = JsonConvert.DeserializeObject<APIResponse>(response.Content.ReadAsStringAsync().Result);
+                    if (apiResponseObject.MessageType == EnumMessageType.OPERATION_SUCCESS)
+                    {
+                       
+                        return View("Index");
                     }
                     else
                     {
